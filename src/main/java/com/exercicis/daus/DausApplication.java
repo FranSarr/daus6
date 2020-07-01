f1e735af0284b8bc4ece81809ffad371ef3aca77
fsarrias <Som=2018>
2020-07-01 19:04:52 +0200

    Empiezo a montar la estructura de datos

---
+++ src/main/java/com/exercicis/daus/DausApplication.java
@@ -0,0 +1,13 @@
+package com.exercicis.daus;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DausApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DausApplication.class, args);
+    }
+
+}
